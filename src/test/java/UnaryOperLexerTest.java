5e21f0be3c24ce3e84a3d75bd39efb90ecda3408
Heal, Louis <louis.heal20@imperial.ac.uk>
2022-02-07 13:32:55 +0000

    test: implement unary operator tests

--- src/test/java/UnaryOperLexerTest.java
+++ src/test/java/UnaryOperLexerTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import antlr.*;
@@ -8,13 +9,93 @@ import org.junit.Test;
 public class UnaryOperLexerTest {
 
   @Test
-  public void numberWithoutSignTokenisedAndParsedCorrectly(){
-    CharStream input = CharStreams.fromString("5");
+  public void unaryOperBangTokenizesCorrectly(){
+
+    String treeResult = "(prog begin (stat print (expr (unaryOper !) "
+                      + "(expr x))) end <EOF>)";
+    String program = "begin\n"
+                   + "print !x\n"
+                   + "end";
+
+    CharStream input = CharStreams.fromString(program);
     BasicLexer lexer = new BasicLexer(input);
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     BasicParser parser = new BasicParser(tokens);
     ParseTree tree = parser.prog();
-    assertTrue(tree.toStringTree(parser).equals("(prog (expr (intLiter 5)) <EOF>)"));
+
+    assertEquals(treeResult, tree.toStringTree(parser));
+  }
+
+  @Test
+  public void unaryOperMinusTokenizesCorrectly(){
+
+    String treeResult = "(prog begin (stat print (expr (unaryOper -) "
+                      + "(expr x))) end <EOF>)";
+    String program = "begin\n"
+                   + "print -x\n"
+                   + "end";
+
+    CharStream input = CharStreams.fromString(program);
+    BasicLexer lexer = new BasicLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    BasicParser parser = new BasicParser(tokens);
+    ParseTree tree = parser.prog();
+
+    assertEquals(treeResult, tree.toStringTree(parser));
+  }
+
+  @Test
+  public void unaryOperLenTokenizesCorrectly(){
+
+    String treeResult = "(prog begin (stat print (expr (unaryOper len) "
+                      + "(expr \"compiler\"))) end <EOF>)";
+    String program = "begin\n"
+                   + "print len \"compiler\"\n"
+                   + "end";
+
+    CharStream input = CharStreams.fromString(program);
+    BasicLexer lexer = new BasicLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    BasicParser parser = new BasicParser(tokens);
+    ParseTree tree = parser.prog();
+
+    assertEquals(treeResult, tree.toStringTree(parser));
+  }
+
+  @Test
+  public void unaryOperOrdTokenizesCorrectly(){
+
+    String treeResult = "(prog begin (stat print (expr (unaryOper ord) "
+                      + "(expr 'c'))) end <EOF>)";
+    String program = "begin\n"
+                   + "print ord 'c'\n"
+                   + "end";
+
+    CharStream input = CharStreams.fromString(program);
+    BasicLexer lexer = new BasicLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    BasicParser parser = new BasicParser(tokens);
+    ParseTree tree = parser.prog();
+
+    assertEquals(treeResult, tree.toStringTree(parser));
+  }
+
+  @Test
+  public void unaryOperChrTokenizesCorrectly(){
+
+    String treeResult = "(prog begin (stat print (expr (unaryOper chr) "
+                      + "(expr 99))) end <EOF>)";
+    String program = "begin\n"
+                   + "print chr 99\n"
+                   + "end";
+
+    CharStream input = CharStreams.fromString(program);
+    BasicLexer lexer = new BasicLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    BasicParser parser = new BasicParser(tokens);
+    ParseTree tree = parser.prog();
+
+    assertEquals(treeResult, tree.toStringTree(parser));
   }
 
 }
