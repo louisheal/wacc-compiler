e37f0af33334e9248bb88495bfe59a9032a783a1
Tahmid <tr920@ic.ac.uk>
2022-02-07 12:49:12 +0000

    test: added BinaryLexerTests and updated previous tests

--- src/test/java/BinaryLexerTest.java
+++ src/test/java/BinaryLexerTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import antlr.*;
@@ -7,131 +8,339 @@ import org.junit.Test;
 
 public class BinaryLexerTest {
 
-  String additionTree = "(prog begin (stat (type (baseType int)) x = (assignRHS (expr (expr 3) "
-                      + "(binaryOper +) (expr 5)))) end <EOF>)";
-  String additionProgram = "begin\n"
-                         + "int x = 3 + 5\n"
-                         + "end\n";
-
   @Test
-  public void addition_test(){
-    CharStream input = CharStreams.fromString(additionProgram);
+  public void addition_test1(){
+    String treeResult = "(prog begin (stat (type (baseType int)) x = (assignRHS (expr (expr 3) "
+        + "(binaryOper +) (expr 5)))) end <EOF>)";
+    String program = "begin\n"
+        + "int x = 3 + 5\n"
+        + "end\n";
+    CharStream input = CharStreams.fromString(program);
     BasicLexer lexer = new BasicLexer(input);
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     BasicParser parser = new BasicParser(tokens);
     ParseTree tree = parser.prog();
-    System.out.println(tree.toStringTree(parser));
-    assertTrue(tree.toStringTree(parser).equals(additionTree));
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
   }
 
   @Test
-  public void subtraction_test(){
-    CharStream input = CharStreams.fromString("9-3");
+  public void addition_test2(){
+    String treeResult = "(prog begin (stat (type (baseType int)) x = " +
+        "(assignRHS (expr (expr (expr 5) (binaryOper +)" +
+        " (expr 4)) (binaryOper +) (expr 2)))) end <EOF>)";
+    String program =  "begin\n" +
+        "int x = 5 + 4 + 2\n" +
+        "end\n";
+    CharStream input = CharStreams.fromString(program);
     BasicLexer lexer = new BasicLexer(input);
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     BasicParser parser = new BasicParser(tokens);
     ParseTree tree = parser.prog();
-    assertTrue(tree.toStringTree(parser).equals("(prog (expr (expr (intLiter 9)) (binaryOper -) "
-        + "(expr (intLiter 3))) <EOF>)"));
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
   }
 
   @Test
-  public void multiplication_test(){
-    CharStream input = CharStreams.fromString("2*6");
+  public void subtraction_test1(){
+    String treeResult = "(prog begin (stat (type (baseType int)) x = " +
+                             "(assignRHS (expr (expr 5) (binaryOper -) (expr 2)))) end <EOF>)";
+    String program = "begin\n" +
+                                "int x = 5 - 2" +
+                                "end\n";
+    CharStream input = CharStreams.fromString(program);
     BasicLexer lexer = new BasicLexer(input);
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     BasicParser parser = new BasicParser(tokens);
     ParseTree tree = parser.prog();
-    assertTrue(tree.toStringTree(parser).equals("(prog (expr (expr (intLiter 2)) (binaryOper *) "
-        + "(expr (intLiter 6))) <EOF>)"));
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
   }
 
   @Test
-  public void division_test(){
-    CharStream input = CharStreams.fromString("8/4");
+  public void subtraction_test2(){
+    String treeResult = "(prog begin (stat (type (baseType int)) x = " +
+                        "(assignRHS (expr (expr (expr 9) (binaryOper -) (expr 4)) (binaryOper -)" +
+                        " (expr 3)))) end <EOF>)";
+    String program =  "begin\n" +
+                      "int x = 9 - 4 - 3\n" +
+                      "end\n";
+    CharStream input = CharStreams.fromString(program);
     BasicLexer lexer = new BasicLexer(input);
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     BasicParser parser = new BasicParser(tokens);
     ParseTree tree = parser.prog();
-    assertTrue(tree.toStringTree(parser).equals("(prog (expr (expr (intLiter 8)) (binaryOper /) "
-        + "(expr (intLiter 4))) <EOF>)"));
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
   }
 
   @Test
-  public void modulo_test(){
-    CharStream input = CharStreams.fromString("7%3");
+  public void multiplication_test1(){
+    String treeResult = "(prog begin (stat (type (baseType int)) x = " +
+                                "(assignRHS (expr (expr 2) (binaryOper *) (expr 6)))) end <EOF>)";
+    String program = "begin\n" +
+                                   "int x = 2 * 6" +
+                                   "end\n";
+    CharStream input = CharStreams.fromString(program);
     BasicLexer lexer = new BasicLexer(input);
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     BasicParser parser = new BasicParser(tokens);
     ParseTree tree = parser.prog();
-    assertTrue(tree.toStringTree(parser).equals("(prog (expr (expr (intLiter 7)) (binaryOper %) "
-        + "(expr (intLiter 3))) <EOF>)"));
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
   }
 
   @Test
-  public void greater_than_test(){
-    CharStream input = CharStreams.fromString("9>8");
+  public void multiplication_test2(){
+    String treeResult = "(prog begin (stat (type (baseType int)) x = " +
+                        "(assignRHS (expr (expr (expr 5) (binaryOper *)" +
+                        " (expr 3)) (binaryOper *) (expr 4)))) end <EOF>)";
+    String program =  "begin\n" +
+                      "int x = 5 * 3 * 4\n" +
+                      "end\n";
+    CharStream input = CharStreams.fromString(program);
     BasicLexer lexer = new BasicLexer(input);
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     BasicParser parser = new BasicParser(tokens);
     ParseTree tree = parser.prog();
-    assertTrue(tree.toStringTree(parser).equals("(prog (expr (expr (intLiter 9)) (binaryOper >) "
-        + "(expr (intLiter 8))) <EOF>)"));
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
+  }
+
+
+  @Test
+  public void division_test1(){
+    String treeResult = "(prog begin (stat (type (baseType int)) x = " +
+                          "(assignRHS (expr (expr 8) (binaryOper /) (expr 4)))) end <EOF>)";
+    String program = "begin\n" +
+                             "int x = 8 / 4\n" +
+                             "end\n";
+    CharStream input = CharStreams.fromString(program);
+    BasicLexer lexer = new BasicLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    BasicParser parser = new BasicParser(tokens);
+    ParseTree tree = parser.prog();
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
   }
 
   @Test
-  public void greater_than_or_equal_test(){
-    CharStream input = CharStreams.fromString("5>=5");
+  public void division_test2(){
+    String treeResult = "(prog begin (stat (type (baseType int)) x = " +
+                        "(assignRHS (expr (expr (expr 8) (binaryOper /) (expr 4)) (binaryOper /) " +
+                        "(expr 2)))) end <EOF>)";
+    String program =  "begin\n" +
+                      "int x = 8 / 4 / 2\n" +
+                      "end";
+    CharStream input = CharStreams.fromString(program);
     BasicLexer lexer = new BasicLexer(input);
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     BasicParser parser = new BasicParser(tokens);
     ParseTree tree = parser.prog();
-    assertTrue(tree.toStringTree(parser).equals("(prog (expr (expr (intLiter 5)) (binaryOper >=) "
-        + "(expr (intLiter 5))) <EOF>)"));
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
   }
 
   @Test
-  public void less_than_test(){
-    CharStream input = CharStreams.fromString("6<9");
+  public void modulo_test1(){
+    String treeResult = "(prog begin (stat (type (baseType int)) x = " +
+                        "(assignRHS (expr (expr 7) (binaryOper %) (expr 3)))) end <EOF>)";
+    String program = "begin\n" +
+                     "int x = 7 % 3\n" +
+                     "end\n";
+    CharStream input = CharStreams.fromString(program);
     BasicLexer lexer = new BasicLexer(input);
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     BasicParser parser = new BasicParser(tokens);
     ParseTree tree = parser.prog();
-    assertTrue(tree.toStringTree(parser).equals("(prog (expr (expr (intLiter 6)) (binaryOper <) "
-        + "(expr (intLiter 9))) <EOF>)"));
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
   }
 
   @Test
-  public void less_than_or_equal_test(){
-    CharStream input = CharStreams.fromString("3<=4");
+  public void modulo_test2(){
+    String treeResult = "(prog begin (stat (type (baseType int)) x = " +
+                        "(assignRHS (expr (expr 200) (binaryOper %) (expr 3)))) end <EOF>)";
+    String program =  "begin\n" +
+                      "int x = 200 % 3\n" +
+                      "end\n";
+    CharStream input = CharStreams.fromString(program);
     BasicLexer lexer = new BasicLexer(input);
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     BasicParser parser = new BasicParser(tokens);
     ParseTree tree = parser.prog();
-    assertTrue(tree.toStringTree(parser).equals("(prog (expr (expr (intLiter 3)) (binaryOper <=) "
-        + "(expr (intLiter 4))) <EOF>)"));
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
   }
 
   @Test
-  public void equal_test(){
-    CharStream input = CharStreams.fromString("4==4");
+  public void greater_than_test1(){
+    String treeResult = "(prog begin (stat (type (baseType bool)) x = " +
+                        "(assignRHS (expr (expr 9) (binaryOper >) (expr 8)))) end <EOF>)";
+    String program =  "begin\n" +
+                      "bool x = 9 > 8\n" +
+                      "end\n";
+    CharStream input = CharStreams.fromString(program);
     BasicLexer lexer = new BasicLexer(input);
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     BasicParser parser = new BasicParser(tokens);
     ParseTree tree = parser.prog();
-    assertTrue(tree.toStringTree(parser).equals("(prog (expr (expr (intLiter 4)) (binaryOper ==) "
-        + "(expr (intLiter 4))) <EOF>)"));
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
   }
 
   @Test
-  public void not_equal_test(){
-    CharStream input = CharStreams.fromString("1!=2");
+  public void greater_than_test2(){
+    String treeResult = "(prog begin (stat (type (baseType bool)) x = " +
+                        "(assignRHS (expr (expr 1) (binaryOper >) (expr 2)))) end <EOF>)";
+    String program =  "begin\n" +
+                      "bool x = 1 > 2\n" +
+                      "end\n";
+    CharStream input = CharStreams.fromString(program);
     BasicLexer lexer = new BasicLexer(input);
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     BasicParser parser = new BasicParser(tokens);
     ParseTree tree = parser.prog();
-    assertTrue(tree.toStringTree(parser).equals("(prog (expr (expr (intLiter 1)) (binaryOper !=) "
-        + "(expr (intLiter 2))) <EOF>)"));
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
+  }
+
+  @Test
+  public void greater_than_or_equal_test1(){
+    String treeResult = "(prog begin (stat (type (baseType bool)) x = " +
+                        "(assignRHS (expr (expr 5) (binaryOper >=) (expr 5)))) end <EOF>)";
+    String program =  "begin\n" +
+                      "bool x = 5 >= 5\n" +
+                      "end\n";
+    CharStream input = CharStreams.fromString(program);
+    BasicLexer lexer = new BasicLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    BasicParser parser = new BasicParser(tokens);
+    ParseTree tree = parser.prog();
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
+  }
+
+  @Test
+  public void greater_than_or_equal_test2(){
+    String treeResult = "(prog begin (stat (type (baseType bool)) x = " +
+                        "(assignRHS (expr (expr 4) (binaryOper >=) (expr 5)))) end <EOF>)";
+    String program =  "begin\n" +
+                      "bool x = 4 >= 5\n" +
+                      "end\n";
+    CharStream input = CharStreams.fromString(program);
+    BasicLexer lexer = new BasicLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    BasicParser parser = new BasicParser(tokens);
+    ParseTree tree = parser.prog();
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
+  }
+
+  @Test
+  public void less_than_test1(){
+    String treeResult = "(prog begin (stat (type (baseType bool)) x = " +
+                        "(assignRHS (expr (expr 6) (binaryOper <) (expr 9)))) end <EOF>)";
+    String program =  "begin\n" +
+                      "bool x = 6 < 9\n" +
+                      "end\n";
+    CharStream input = CharStreams.fromString(program);
+    BasicLexer lexer = new BasicLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    BasicParser parser = new BasicParser(tokens);
+    ParseTree tree = parser.prog();
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
+  }
+
+  @Test
+  public void less_than_test2(){
+    String treeResult = "(prog begin (stat (type (baseType bool)) x = " +
+                        "(assignRHS (expr (expr 10) (binaryOper <) (expr 9)))) end <EOF>)";
+    String program =  "begin\n" +
+                      "bool x = 10 < 9\n" +
+                      "end\n";
+    CharStream input = CharStreams.fromString(program);
+    BasicLexer lexer = new BasicLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    BasicParser parser = new BasicParser(tokens);
+    ParseTree tree = parser.prog();
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
+  }
+
+  @Test
+  public void less_than_or_equal_test1(){
+    String treeResult = "(prog begin (stat (type (baseType bool)) x = " +
+        "(assignRHS (expr (expr 3) (binaryOper <=) (expr 4)))) end <EOF>)";
+    String program =  "begin\n" +
+        "bool x = 3 <= 4\n" +
+        "end\n";
+    CharStream input = CharStreams.fromString(program);
+    BasicLexer lexer = new BasicLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    BasicParser parser = new BasicParser(tokens);
+    ParseTree tree = parser.prog();
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
+  }
+
+  @Test
+  public void less_than_or_equal_test2(){
+    String treeResult = "(prog begin (stat (type (baseType bool)) x = " +
+                        "(assignRHS (expr (expr 9) (binaryOper <=) (expr 4)))) end <EOF>)";
+    String program =  "begin\n" +
+                      "bool x = 9 <= 4\n" +
+                      "end\n";
+    CharStream input = CharStreams.fromString(program);
+    BasicLexer lexer = new BasicLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    BasicParser parser = new BasicParser(tokens);
+    ParseTree tree = parser.prog();
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
+  }
+
+  @Test
+  public void equal_test1(){
+    String treeResult = "(prog begin (stat (type (baseType bool)) x = " +
+                        "(assignRHS (expr (expr 4) (binaryOper ==) (expr 4)))) end <EOF>)";
+    String program =  "begin\n" +
+                      "bool x = 4 == 4\n" +
+                      "end\n";
+    CharStream input = CharStreams.fromString(program);
+    BasicLexer lexer = new BasicLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    BasicParser parser = new BasicParser(tokens);
+    ParseTree tree = parser.prog();
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
+  }
+
+  @Test
+  public void equal_test2(){
+    String treeResult = "(prog begin (stat (type (baseType bool)) x = " +
+                        "(assignRHS (expr (expr 2) (binaryOper ==) (expr 1)))) end <EOF>)";
+    String program =  "begin\n" +
+        "bool x = 2 == 1\n" +
+        "end\n";
+    CharStream input = CharStreams.fromString(program);
+    BasicLexer lexer = new BasicLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    BasicParser parser = new BasicParser(tokens);
+    ParseTree tree = parser.prog();
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
+  }
+
+  @Test
+  public void not_equal_test1(){
+    String treeResult = "(prog begin (stat (type (baseType bool)) x = " +
+                        "(assignRHS (expr (expr 1) (binaryOper !=) (expr 2)))) end <EOF>)";
+    String program =  "begin\n" +
+                      "bool x = 1 != 2\n" +
+                      "end\n";
+    CharStream input = CharStreams.fromString(program);
+    BasicLexer lexer = new BasicLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    BasicParser parser = new BasicParser(tokens);
+    ParseTree tree = parser.prog();
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
+  }
+
+  @Test
+  public void not_equal_test2(){
+    String treeResult = "(prog begin (stat (type (baseType bool)) x = " +
+                        "(assignRHS (expr (expr 2) (binaryOper !=) (expr 2)))) end <EOF>)";
+    String program =  "begin\n" +
+                      "bool x = 2 != 2\n" +
+                      "end\n";
+    CharStream input = CharStreams.fromString(program);
+    BasicLexer lexer = new BasicLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    BasicParser parser = new BasicParser(tokens);
+    ParseTree tree = parser.prog();
+    assertTrue(tree.toStringTree(parser).equals(treeResult));
   }
 
 
