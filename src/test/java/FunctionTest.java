30f9fc573424ae02ea107fdd16149d89aba5a15f
Louis Heal <lh420@cloud-vm-43-227.doc.ic.ac.uk>
2022-02-07 21:41:46 +0000

    test: test that functions with parameters tokenise and parse correctly

--- src/test/java/FunctionTest.java
+++ src/test/java/FunctionTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 public class FunctionTest {
 
     @Test
-    public void unaryOperBangTokenizesCorrectly(){
+    public void basicFunctionTokenizesAndParsesCorrectly(){
 
         String treeResult = "(prog begin (func (type (baseType int)) "
                           + "foo ( ) is (stat skip) end) (stat skip) end <EOF>)";
@@ -17,4 +17,22 @@ public class FunctionTest {
         assertEquals(treeResult, Compiler.lexAnalyse(program));
     }
 
+    @Test
+    public void functionWithParamsTokenizesAndParsesCorrectly(){
+
+        String treeResult = "(prog begin (func (type (baseType int)) "
+                          + "foo ( (paramList (param (type (baseType int)) x) , "
+			  + "(param (type (baseType int)) y)) ) is (stat skip) end) " 
+			  + "(stat skip) end <EOF>)";
+        String program = "begin\n"
+                       + "int foo (int x, int y) is skip end\n"
+                       + "skip\n"
+                       + "end";
+
+	System.out.println(Compiler.lexAnalyse(program));
+
+        assertEquals(treeResult, Compiler.lexAnalyse(program));
+    }
+
 }
+
