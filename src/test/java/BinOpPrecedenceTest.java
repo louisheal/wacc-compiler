78fe3a104fb6e8c4384ff20f875c9fc87bf19da4
Heal, Louis <louis.heal20@imperial.ac.uk>
2022-02-08 12:25:26 +0000

    test: implement precedence tests for binary operators

--- src/test/java/BinOpPrecedenceTest.java
+++ src/test/java/BinOpPrecedenceTest.java
@@ -1,2 +1,62 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BinOpPrecedenceTest {
+
+  @Test
+  public void addThenMultiplyHasCorrectOrdering() {
+
+    String treeResult = "(prog begin (stat (type (baseType int)) x = " +
+            "(assignRHS (expr (expr (intLiter 5)) (binaryOper +) " +
+            "(expr (expr (intLiter 3)) (binaryOper *) (expr (intLiter 9)))" +
+            "))) end <EOF>)";
+    String program = "begin\n"
+            + "int x = 5 + 3 * 9\n"
+            + "end";
+
+    assertEquals(treeResult, Compiler.lexAnalyse(program));
+  }
+
+  @Test
+  public void subtractThenMultiplyHasCorrectOrdering() {
+
+    String treeResult = "(prog begin (stat (type (baseType int)) x = " +
+            "(assignRHS (expr (expr (intLiter 5)) (binaryOper -) " +
+            "(expr (expr (intLiter 3)) (binaryOper *) (expr (intLiter 9)))" +
+            "))) end <EOF>)";
+    String program = "begin\n"
+            + "int x = 5 - 3 * 9\n"
+            + "end";
+
+    assertEquals(treeResult, Compiler.lexAnalyse(program));
+  }
+
+  @Test
+  public void addThenDivideHasCorrectOrdering() {
+
+    String treeResult = "(prog begin (stat (type (baseType int)) x = " +
+            "(assignRHS (expr (expr (intLiter 5)) (binaryOper +) " +
+            "(expr (expr (intLiter 9)) (binaryOper /) (expr (intLiter 3)))" +
+            "))) end <EOF>)";
+    String program = "begin\n"
+            + "int x = 5 + 9 / 3\n"
+            + "end";
+
+    assertEquals(treeResult, Compiler.lexAnalyse(program));
+  }
+
+  @Test
+  public void subtractThenDivideHasCorrectOrdering() {
+
+    String treeResult = "(prog begin (stat (type (baseType int)) x = " +
+            "(assignRHS (expr (expr (intLiter 5)) (binaryOper -) " +
+            "(expr (expr (intLiter 9)) (binaryOper /) (expr (intLiter 3)))" +
+            "))) end <EOF>)";
+    String program = "begin\n"
+            + "int x = 5 - 9 / 3\n"
+            + "end";
+
+    assertEquals(treeResult, Compiler.lexAnalyse(program));
+  }
 }
