769231e1bc56bc6ae3c12f26a2fcae993b38ca68
Heal, Louis <louis.heal20@imperial.ac.uk>
2022-02-10 15:11:12 +0000

    feat: implement exprType, intLiter, boolLiter, charLiter, stringLiter, ident, arrayElem, expression1 and expression2 fields in Expression class

--- src/main/java/ast/Expression.java
+++ src/main/java/ast/Expression.java
@@ -2,6 +2,17 @@ package ast;
 
 public class Expression {
 
+  ExprType exprType;
+  int intLiter;
+  boolean boolLiter;
+  char charLiter;
+  String stringLiter;
+  // PAIR
+  String ident;
+  ArrayElem arrayElem;
+  Expression expression1;
+  Expression expression2;
+
   enum ExprType {
 
     INTLITER,
