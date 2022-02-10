e498ba83fedc5511f1c93e7e49a68336cf1821f4
Heal, Louis <louis.heal20@imperial.ac.uk>
2022-02-10 14:47:31 +0000

    feat: implement stattype, lhstype, lhsident, lhs, rhs, expression, statement1 and statement2 fields in statement class

--- src/main/java/ast/Statement.java
+++ src/main/java/ast/Statement.java
@@ -2,6 +2,15 @@ package ast;
 
 public class Statement {
 
+  StatType statType;
+  Type lhsType;
+  String lhsIdent;
+  AssignLHS lhs;
+  AssignRHS rhs;
+  Expression expression;
+  Statement statement1;
+  Statement statement2;
+
   enum StatType {
 
     SKIP,
