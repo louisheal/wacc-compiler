03275dac172c55e4018f19fdb19eec3294d35a7b
Heal, Louis <louis.heal20@imperial.ac.uk>
2022-02-10 14:44:13 +0000

    feat: implement StatType enum in statement class

--- src/main/java/ast/Statement.java
+++ src/main/java/ast/Statement.java
@@ -1,4 +1,24 @@
 package ast;
 
 public class Statement {
+
+  enum StatType {
+
+    SKIP,
+    DECLARATION,
+    REASSIGNMENT,
+    READ,
+    FREE,
+    RETURN,
+    EXIT,
+    PRINT,
+    PRINTLN,
+    IF,
+    WHILE,
+    BEGIN,
+    CONCAT
+
+  }
+
 }
+
