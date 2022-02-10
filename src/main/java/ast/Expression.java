1ddfd6ce884d16ccc383c10b227bb7177c3c3e43
Heal, Louis <louis.heal20@imperial.ac.uk>
2022-02-10 15:06:33 +0000

    feat: implement ExprType enum to define the type of an Expression object

--- src/main/java/ast/Expression.java
+++ src/main/java/ast/Expression.java
@@ -1,4 +1,36 @@
 package ast;
 
 public class Expression {
+
+  enum ExprType {
+
+    INTLITER,
+    BOOLLITER,
+    CHARLITER,
+    STRINGLITER,
+    PAIRLITER,
+    IDENT,
+    ARRAYELEM,
+    NOT,
+    UN_MINUS,
+    LEN,
+    ORD,
+    CHR,
+    DIVIDE,
+    MULTIPLY,
+    MODULO,
+    PLUS,
+    BIN_MINUS,
+    GT,
+    GTE,
+    LT,
+    LTE,
+    EQ,
+    NEQ,
+    AND,
+    OR,
+    BRACKETS
+
+  }
+
 }
