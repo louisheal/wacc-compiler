c52029109248aa72b4ec97a318350f782faae47d
Heal, Louis <louis.heal20@imperial.ac.uk>
2022-02-10 15:03:48 +0000

    feat: implement assignType, expression1, expression2, array, pairElem, functionIdent and argList fields in AssignRHS class

--- src/main/java/ast/AssignRHS.java
+++ src/main/java/ast/AssignRHS.java
@@ -1,6 +1,16 @@
 package ast;
 
+import java.util.List;
+
 public class AssignRHS {
+
+  RHSType assignType;
+  Expression expression1;
+  Expression expression2;
+  List<Expression> array;
+  PairElem pairElem;
+  String functionIdent;
+  List<Argument> argList;
 
   enum RHSType {
 
