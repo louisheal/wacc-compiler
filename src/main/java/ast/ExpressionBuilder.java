358c19193f170104ae38a3cff4bde8a9fd3bbef1
Heal, Louis <louis.heal20@imperial.ac.uk>
2022-02-11 03:28:45 +0000

    feat: implemented more constructors to the ExpressionBuilder class

--- src/main/java/ast/ExpressionBuilder.java
+++ src/main/java/ast/ExpressionBuilder.java
@@ -18,6 +18,46 @@ public class ExpressionBuilder {
     return this.build();
   }
 
+  public Expression buildBoolExpr(boolean boolLiter) {
+    this.exprType = Expression.ExprType.BOOLLITER;
+    this.boolLiter = boolLiter;
+    return this.build();
+  }
+
+  public Expression buildCharExpr(char charLiter) {
+    this.exprType = Expression.ExprType.CHARLITER;
+    this.charLiter = charLiter;
+    return this.build();
+  }
+
+  public Expression buildIdentExpr(String ident) {
+    this.exprType = Expression.ExprType.IDENT;
+    this.ident = ident;
+    return this.build();
+  }
+
+  public Expression buildArrayExpr(ArrayElem arrayElem) {
+    this.exprType = Expression.ExprType.ARRAYELEM;
+    this.arrayElem = arrayElem;
+    return this.build();
+  }
+
+  //TODO: UnOp Expressions
+
+  //TODO: IntOp Expressions
+
+  //TODO: IntCharOpExpressions
+
+  //TODO: AllOpExpressions
+
+  //TODO: BoolOpExpressions
+
+  public Expression buildBracketsExpr(Expression expression) {
+    this.exprType = Expression.ExprType.BRACKETS;
+    this.expression1 = expression;
+    return this.build();
+  }
+
   private Expression build() {
     return new Expression(exprType, intLiter, boolLiter, charLiter, stringLiter, ident, arrayElem, expression1, expression2);
   }
