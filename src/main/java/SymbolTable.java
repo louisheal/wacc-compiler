975755b747141a042762c173cd20c6c82e9b5ab2
Tahmid <tr920@ic.ac.uk>
2022-02-11 06:45:26 +0000

    feat: began type checking of statements in traversal

--- src/main/java/SymbolTable.java
+++ src/main/java/SymbolTable.java
@@ -1,3 +1,4 @@
+import ast.Type;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
 import java.util.HashMap;
@@ -6,17 +7,19 @@ import java.util.Map;
 public class SymbolTable {
 
   private final SymbolTable parent;
-  private final Map<String, SemanticChecker.Type> variables = new HashMap<>();
+  private final Map<String, Type.EType> variables = new HashMap<>();
 
   public SymbolTable(SymbolTable parent) {
     this.parent = parent;
   }
 
-  public void newSymbol(String ident, SemanticChecker.Type node) {
+  public boolean contains(String ident) {return variables.containsKey(ident);}
+
+  public void newSymbol(String ident, Type.EType node) {
     variables.put(ident, node);
   }
 
-  public SemanticChecker.Type getType(String ident) {
+  public Type.EType getType(String ident) {
     return variables.get(ident);
   }
 
--- src/main/java/TraverseAST.java
+++ src/main/java/TraverseAST.java
@@ -1,8 +1,18 @@
 import ast.Function;
+import ast.Param;
 import ast.Program;
 import ast.Statement;
+import ast.Type;
+import java.util.List;
 
 public class TraverseAST {
+  SymbolTable currentST = new SymbolTable(null);
+
+  private int errors = 0;
+
+  public Integer getNumberOfErrors() {
+    return errors;
+  }
 
   public void traverse(Program program) {
     for (Function function : program.getFunctions()) {
@@ -12,11 +22,46 @@ public class TraverseAST {
   }
 
   private void traverse(Function function) {
+    traverse(function.getParams());
+    traverse(function.getStatement());
+  }
+
+  private void traverse(List<Param> params) {
 
   }
 
   private void traverse(Statement statement) {
+    switch (statement.getStatType()) {
+      case SKIP:
+        break;
+      case DECLARATION:
+        currentST.newSymbol(statement.getLhsIdent(), statement.getLhsType().getType());
+      case REASSIGNMENT:
+        Type.EType lhsType = currentST.getType(statement.getLhsIdent());
+        statement.getRHS();
+        // need to check if rhs has same type as lhs
 
+      case READ:
+
+      case FREE:
+
+      case RETURN:
+
+      case EXIT:
+
+      case PRINT:
+
+      case PRINTLN:
+
+      case IF:
+
+      case WHILE:
+
+      case BEGIN:
+
+      case CONCAT:
+        traverse(statement.getStatement1());
+        traverse(statement.getStatement2());
+    }
   }
-
 }
