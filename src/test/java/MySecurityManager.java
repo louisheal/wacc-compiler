61dc7a374c02a3f8c8f9f0d037e8db5616230198
Tahmid Rahman <tr920@ic.ac.uk>
2022-02-16 23:27:25 +0000

    chore: explaining the use of security manager

--- src/test/java/MySecurityManager.java
+++ src/test/java/MySecurityManager.java
@@ -4,6 +4,8 @@ public class MySecurityManager extends SecurityManager {
 
   @Override
   public void checkExit(int status) {
+    // Since semantic and syntax errors send exit status of 100/200, we can catch them in the test
+    // to send an error out for the specific tests
     if (status == 100 || status == 200) {
       throw new SecurityException();
     }
