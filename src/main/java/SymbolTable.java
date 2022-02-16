d54455775a2424620923d885a8236ac0b2cf7e7c
Tahmid Rahman <tr920@ic.ac.uk>
2022-02-16 23:51:03 +0000

    feat: updated symbol table to include functions and their parameters

--- src/main/java/SymbolTable.java
+++ src/main/java/SymbolTable.java
@@ -1,5 +1,7 @@
 import antlr.BasicParser;
+import ast.Param;
 import ast.Type;
+import java.util.List;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
 import java.util.HashMap;
@@ -9,6 +11,9 @@ public class SymbolTable {
 
   private final SymbolTable parent;
   private final Map<String, Type> variables = new HashMap<>();
+  private final Map<String, List<Param>> functionToParams = new HashMap<>();
+  private final Map<String, Type.EType> functionToReturnType = new HashMap<>();
+
 
   public SymbolTable(SymbolTable parent) {
     this.parent = parent;
@@ -20,6 +25,14 @@ public class SymbolTable {
 
   public boolean contains(String ident) {
     return variables.containsKey(ident);
+  }
+
+  public List<Param> getFunctionParams(String ident) {
+    return functionToParams.get(ident);
+  }
+
+  public Type.EType getFunctionReturnType(String ident) {
+    return functionToReturnType.get(ident);
   }
 
   public Type getType(String ident) {
