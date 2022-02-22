f8fb11fce86e01f1012540fb586eb8afc3c5992a
Tahmid Rahman <tr920@ic.ac.uk>
2022-02-22 09:35:30 +0000

    feat: created Condtionals enum class with all conditionals cases

---
+++ src/main/java/assembly/Conditionals.java
@@ -0,0 +1,10 @@
+package assembly;
+
+public enum Conditionals {
+  EQ, // Equal to
+  NE, // Not equal to
+  LE, // Less than or equal to
+  LT, // Less than
+  GE, // Greater than or equal to
+  GT, // Greater than
+}
