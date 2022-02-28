0b1e8636034ce98a58c3bb3b0d0ac41aed4d6161
Louis Heal <lh420@ic.ac.uk>
2022-02-28 13:28:51 +0000

    feat: implement register allocation for expressions: > and >=

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -600,16 +600,23 @@ public class Converter extends ASTVisitor<List<Instruction>> {
     /* Generate assembly code to evaluate both expressions and store them in Rn, Rn+1. */
     List<Instruction> instructions = translateBinaryExpression(expression);
 
+    /* Allocate two registers: rn and rm (rn+1) for this function to use. */
+    Register rn = popUnusedRegister();
+    Register rm = popUnusedRegister();
+
     // CMP Rn, Rn+1
-    instructions.add(new Instruction(InstrType.CMP, unusedRegisters.get(1),
-        new Operand2(unusedRegisters.get(2))));
+    instructions.add(new Instruction(InstrType.CMP, rn, new Operand2(rm)));
 
     // MOVLE Rn, #0
-    instructions.add(new Instruction(InstrType.MOV, unusedRegisters.get(1), 0));
+    instructions.add(new Instruction(InstrType.MOV, rn, 0));
 
     // MOVGT Rn, #1
     //TODO: CREATE CONDITION CODE ENUMS
-    instructions.add(new Instruction(InstrType.MOV, unusedRegisters.get(1), 1));
+    instructions.add(new Instruction(InstrType.MOV, rn, 1));
+
+    /* Mark the two registers used in the evaluation of this function as no longer in use. */
+    pushUnusedRegister(rn);
+    pushUnusedRegister(rm);
 
     return instructions;
   }
@@ -620,16 +627,23 @@ public class Converter extends ASTVisitor<List<Instruction>> {
     /* Generate assembly code to evaluate both expressions and store them in Rn, Rn+1. */
     List<Instruction> instructions = translateBinaryExpression(expression);
 
+    /* Allocate two registers: rn and rm (rn+1) for this function to use. */
+    Register rn = popUnusedRegister();
+    Register rm = popUnusedRegister();
+
     // CMP Rn, Rn+1
-    instructions.add(new Instruction(InstrType.CMP, unusedRegisters.get(1),
-        new Operand2(unusedRegisters.get(2))));
+    instructions.add(new Instruction(InstrType.CMP, rn, new Operand2(rm)));
 
     // MOVLT Rn, #0
-    instructions.add(new Instruction(InstrType.MOV, unusedRegisters.get(1), 0));
+    instructions.add(new Instruction(InstrType.MOV, rn, 0));
 
     // MOVGE Rn, #1
     //TODO: CREATE CONDITION CODE ENUMS
-    instructions.add(new Instruction(InstrType.MOV, unusedRegisters.get(1), 1));
+    instructions.add(new Instruction(InstrType.MOV, rn, 1));
+
+    /* Mark the two registers used in the evaluation of this function as no longer in use. */
+    pushUnusedRegister(rn);
+    pushUnusedRegister(rm);
 
     return instructions;
   }
