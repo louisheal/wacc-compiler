e5a4a025cd1b2029e63cfc0caf7d8be25fb2a642
Daniel Ong <dyo20@ic.ac.uk>
2022-02-28 15:52:42 +0000

    chore: added warning message for LDR

--- src/main/java/assembly/Instruction.java
+++ src/main/java/assembly/Instruction.java
@@ -148,6 +148,7 @@ public class Instruction {
     }
 
     if (type == InstrType.LDR) { //LDR instruction format
+      //TODO: double check if if statements follow correctly
       if(label != null) {
         return type + flag + " " + dest + ", =" + label;
       } else if (operand2 == null) {
