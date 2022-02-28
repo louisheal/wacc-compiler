fd8ba611078d88fde683e841c3ce9fe95e7ee91b
Louis Heal <lh420@ic.ac.uk>
2022-02-28 13:17:32 +0000

    docs: add comments to explain the use of register allocation in visitLessEqExp

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -660,6 +660,7 @@ public class Converter extends ASTVisitor<List<Instruction>> {
     /* Generate assembly code to evaluate both expressions and store them in Rn, Rn+1. */
     List<Instruction> instructions = translateBinaryExpression(expression);
 
+    /* Allocate two registers: rn and rm for this function to use. */
     Register rn = popUnusedRegister();
     Register rm = popUnusedRegister();
 
@@ -673,6 +674,7 @@ public class Converter extends ASTVisitor<List<Instruction>> {
     //TODO: CREATE CONDITION CODE ENUMS
     instructions.add(new Instruction(InstrType.MOV, rn, 1));
 
+    /* Mark the two registers used in the evaluation of this function as no longer in use. */
     pushUnusedRegister(rn);
     pushUnusedRegister(rm);
 
