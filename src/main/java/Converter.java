2861d124e1ead1c00a7843b909f6b053e48df665
Louis Heal <lh420@ic.ac.uk>
2022-02-28 22:22:35 +0000

    feat: implement condition codes into LessEq (<=) expression

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -880,11 +880,10 @@ public class Converter extends ASTVisitor<List<Instruction>> {
     instructions.add(new Instruction(InstrType.CMP, rn, new Operand2(rm)));
 
     // MOVGT Rn, #0
-    instructions.add(new Instruction(InstrType.MOV, rn, 0));
+    instructions.add(new Instruction(InstrType.MOV, rn, 0, Conditionals.GT));
 
     // MOVLE Rn, #1
-    //TODO: CREATE CONDITION CODE ENUMS
-    instructions.add(new Instruction(InstrType.MOV, rn, 1));
+    instructions.add(new Instruction(InstrType.MOV, rn, 1, Conditionals.LE));
 
     /* Mark the two registers used in the evaluation of this function as no longer in use. */
     pushUnusedRegister(rm);
