e88e84c969e7f6b1aa1268ef276a2196a6de382e
Louis Heal <lh420@ic.ac.uk>
2022-02-28 22:25:37 +0000

    feat: implement condition codes into Less (<) expression

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -853,11 +853,10 @@ public class Converter extends ASTVisitor<List<Instruction>> {
     instructions.add(new Instruction(InstrType.CMP, rn, new Operand2(rm)));
 
     // MOVGE Rn, #0
-    instructions.add(new Instruction(InstrType.MOV, rn, 0));
+    instructions.add(new Instruction(InstrType.MOV, rn, 0, Conditionals.GE));
 
     // MOVLT Rn, #1
-    //TODO: CREATE CONDITION CODE ENUMS
-    instructions.add(new Instruction(InstrType.MOV, rn, 1));
+    instructions.add(new Instruction(InstrType.MOV, rn, 1, Conditionals.LT));
 
     /* Mark the two registers used in the evaluation of this function as no longer in use. */
     pushUnusedRegister(rm);
