9f540e49c17c268e41e933f9ec1dc261b0a039f8
Louis Heal <lh420@ic.ac.uk>
2022-03-04 00:11:32 +0000

    feat: reverse registers within div and mod (/ and %) when on the last register

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1041,6 +1041,16 @@ public class Converter extends ASTVisitor<List<Instruction>> {
     Register rn = popUnusedRegister();
     Register rm = popUnusedRegister();
 
+    // TODO: Is this a strong enough check?
+    /* Swap registers for accumulator register allocation. */
+    if (Objects.equals(rn.toString(), "r10")) {
+      // MOV R0, Rn+1
+      instructions.add(new Instruction(MOV, r0, new Operand2(rm)));
+
+      // MOV R1, Rn
+      instructions.add(new Instruction(MOV, r1, new Operand2(rn)));
+    }
+
     // MOV R0, Rn
     instructions.add(new Instruction(MOV, r0, new Operand2(rn)));
 
@@ -1075,6 +1085,16 @@ public class Converter extends ASTVisitor<List<Instruction>> {
     /* Allocate two registers: rn and rm (rn+1) for this function to use. */
     Register rn = popUnusedRegister();
     Register rm = popUnusedRegister();
+
+    // TODO: Is this a strong enough check?
+    /* Swap registers for accumulator register allocation. */
+    if (Objects.equals(rn.toString(), "r10")) {
+      // MOV R0, Rn+1
+      instructions.add(new Instruction(MOV, r0, new Operand2(rm)));
+
+      // MOV R1, Rn
+      instructions.add(new Instruction(MOV, r1, new Operand2(rn)));
+    }
 
     // MOV R0, Rn
     instructions.add(new Instruction(MOV, r0, new Operand2(rn)));
