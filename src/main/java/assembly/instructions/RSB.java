77027e5b37893096b0d1e74f23eac61ed63a5d8f
Louis Heal <lh420@ic.ac.uk>
2022-03-15 14:48:40 +0000

    fix: remove redundant paramter in constructor

--- src/main/java/assembly/instructions/RSB.java
+++ src/main/java/assembly/instructions/RSB.java
@@ -7,14 +7,14 @@ import assembly.Register;
 public class RSB extends Instruction {
 
   //RSB{cond} dest, operand1, operand2
-  public RSB(assembly.Instruction.InstrType type, Register dest, Register operand1, Operand2 operand2) {
+  public RSB(Register dest, Register operand1, Operand2 operand2) {
     this.dest = dest;
     this.operand1 = operand1;
     this.operand2 = operand2;
   }
 
   //RSB{S}{cond} dest, operand1, operand2
-  public RSB(assembly.Instruction.InstrType type, Register dest, Register operand1, Operand2 operand2, Flags flag) {
+  public RSB(Register dest, Register operand1, Operand2 operand2, Flags flag) {
     this.dest = dest;
     this.operand1 = operand1;
     this.operand2 = operand2;
