a06d2db3b495571762bcfca55cb993f1afd1e844
Heal, Louis <louis.heal20@imperial.ac.uk>
2022-03-18 18:59:15 +0000

    Update Compiler.java

--- src/main/java/Compiler.java
+++ src/main/java/Compiler.java
@@ -90,9 +90,9 @@ public class Compiler {
 
   public static void main(String[] args) throws IOException {
 
-    Path filename = Path.of("wacc_examples/valid/expressions/andExpr.wacc");
+    Path filename = Path.of(args[0]);
 
-    Program ast = compile(new String("wacc_examples/valid/expressions/andExpr.wacc"));
+    Program ast = compile(new String(args[0]));
 
     Evaluator evaluator = new Evaluator();
     evaluator.visitProgram(ast);
